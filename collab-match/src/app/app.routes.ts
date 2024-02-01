eca9090a8f3ba25b6fae3fb1514e3456ecad9fab
Esther White <monacodelisa@gmail.com>
2024-02-01 21:38:34 +0200

    dummy user

--- collab-match/src/app/app.routes.ts
+++ collab-match/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { DummyUserComponent } from './components/dummy-user/dummy-user.component';
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'dummy-user', component: DummyUserComponent}
 ];
---
+++ collab-match/src/app/components/dummy-user/dummy-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dummy-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './dummy-user.component.html',
+  styleUrl: './dummy-user.component.scss'
+})
+export class DummyUserComponent {
+
+}
